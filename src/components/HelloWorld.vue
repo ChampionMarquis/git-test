70050d64f18fc1cf9d9102e29cb4ec7619b994b0
sugars <sugars.su@celnet.com.cn>
2018-04-20 12:08:31 +0800

    初始化demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <div>id：{{text.id}}</div>
+    <div>
+      <span>内容：</span>
+      <input type="text" v-model="text.content">
+    </div>
+    <div>
+      <span>位置：</span>
+      <select name="select">
+        <option v-for="v in text.postion" :value="v">{{v}}</option>
+      </select>
+    </div>
+
+    <button @click="toJson">输出JSON</button>
+    <p>模拟输出json：{{json}}</p>
+
+    <button @click="routeTo">跳转到另一个视图Test</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        text: {
+          id: 1,
+          content: '这是初始内容，',
+          postion: ['left', 'center', 'right']
+        },
+        json: ''
+      }
+    },
+    created() {
+      // 组件生命周期--创建时
+      console.log('这个组件创建时触发');
+    },
+    methods: {
+      toJson() {
+        this.json = JSON.stringify(this.text)
+      },
+      routeTo() {
+        this.$router.push({path: '/test'})
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>这是test页面</div>
+    <button @click="backTo">返回</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "test",
+    methods: {
+      backTo() {
+        this.$router.go(-1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
